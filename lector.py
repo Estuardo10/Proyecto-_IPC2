0687829133a9bf6b63e4288f546ab3d4bd485089
Estuardo <3006681710101@ingenieria.usac.edu.gt>
2022-08-26 02:35:26 -0600

    Menú y lectura de xml

--- lector.py
+++ lector.py
@@ -1,10 +1,23 @@
 import xml.etree.ElementTree as ET
 
+def lectura_xml():
+    ruta = input("Ingresa una ruta:")
+    archivo = ET.parse(ruta)
 
-ruta = input("Ingresa una ruta:")
-archivo = ET.parse(ruta)
+    raiz = archivo.getroot()
 
-raiz = archivo.getroot()
+    for hijo in raiz:
+        print(hijo)
 
-for hijo in raiz:
-    print(hijo)
+def menu():
+    while entrada != "3":
+        print("====== Menú =====")
+        print("1. Cargar Archivo")
+        print("2. Seleccionar paciente")
+        print("3. Salir")
+        entrada = input("Ingresa una opción del menú")
+
+        if entrada == "1":
+            lectura_xml() 
+        elif entrada == "2":
+            print("acá método para paciente")
